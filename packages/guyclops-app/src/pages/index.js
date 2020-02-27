a35047b53fc576002844b153832f087fccb98a66
juntae <wnsxo0816@gmail.com>
2020-02-27 15:32:59 +0900

    chore: @guyclops/app add dependency @guyclops/ui

--- packages/guyclops-app/src/pages/index.js
+++ packages/guyclops-app/src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Button } from "../../../guyclops-ui/src"
+import { Button } from "@guyclops/ui"
 
 const IndexPage = () => (
   <Layout>
